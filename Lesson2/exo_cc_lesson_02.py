e35118ded4911781cb3ffffa3040f0d06f423dec
sara <sara.kazdagli@gmail.com>
2018-10-14 22:29:55 +0200

    with pages

--- Lesson2/exo_cc_lesson_02.py
+++ Lesson2/exo_cc_lesson_02.py
@@ -1,36 +1,59 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-urlAcerPCBureau = 'https://www.darty.com/nav/recherche?s=relevence&text=acer&fa=790'
-urlDellPCBureau = 'https://www.darty.com/nav/recherche?s=relevence&text=dell&fa=790'
-
+prefix = 'https://www.darty.com/nav/achat/informatique/ordinateur_portable/portable/marque__'
 
 def _convert_string_to_int(string):
     if "%" in string:
-        test = string.replace('%', '')  #
+        number = string.replace('%', '')  #
         if "-" in string:
-            test2 = test.replace('- ', '')
-    return int(test2)
+            numberInt = number.replace('- ', '')
+    return int(numberInt)
 
 
 def moyenne(list):
     s = 0
     for elem in list:
         s = s + elem
-    return (s / len(list))
+    if len(list) != 0:
+        return (s / len(list))
+    else:
+        return 0
 
 
 def moyenneDesReduc(url):
+    i = 1
+    listOfPages = []
+    list = []
+
     page = requests.get(url)
     if page.status_code == 200:
         soup = BeautifulSoup(page.content, 'html.parser')
+        currentPage = soup.find(class_='darty_product_list_pages_list')
+        if (currentPage):
+            listOfPages = re.findall('\d', currentPage.get_text())
         reduction_ordi = soup.find_all(class_='darty_prix_barre_remise darty_small separator_top')
         if (reduction_ordi):
-            list = []
             for elem in reduction_ordi:
                 list.append(_convert_string_to_int(elem.get_text()))
-            return (moyenne(list))
+    while i < len(listOfPages):
+        if i > 1:
+            url = url.replace('marque_' + str(i), 'marque_' + str(i + 1))
+        else:
+            url = url.replace('marque', 'marque_' + str(i + 1))
+        page = requests.get(url)
+        if page.status_code == 200:
+            soup = BeautifulSoup(page.content, 'html.parser')
+            reduction_ordi = soup.find_all(class_='darty_prix_barre_remise darty_small separator_top')
+            if (reduction_ordi):
+                for elem in reduction_ordi:
+                    list.append(_convert_string_to_int(elem.get_text()))
+        i = i + 1
+    return moyenne(list)
 
 
-print('Pour Acer les reduc sont de l\'ordre de ', moyenneDesReduc(urlAcerPCBureau))
-print('Pour Dell les reduc sont de l\'ordre de ', moyenneDesReduc(urlDellPCBureau))
+ListMarques = ['Acer', 'Dell', 'HP', 'Asus', 'Apple']
+for marque in ListMarques:
+    url = prefix + marque.lower() + '__' + marque.upper() + '.html'
+    print('Pour ' + marque + ' les reduc sont de l\'ordre de ', moyenneDesReduc(url))
